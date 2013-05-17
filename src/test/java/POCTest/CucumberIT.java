723b6a80cc27a47d26ff074a8bbdb5cb69d95fde
letshaveyer <letshaveyer@yahoo.com>
2013-05-17 15:24:00 +0200

    Update CucumberIT.java
    Updated to use new packages in latest version of cucumber

--- src/test/java/POCTest/CucumberIT.java
+++ src/test/java/POCTest/CucumberIT.java
@@ -1,10 +1,10 @@
-package POCTest;
+package poctest;
 
 import org.junit.runner.RunWith;
-import cucumber.junit.Cucumber;
+import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @Cucumber.Options(format = { "json", "json:target/cucumber.json" }, features = { "." }, strict = true)
 public class CucumberIT {
 
-}	 
+}  
